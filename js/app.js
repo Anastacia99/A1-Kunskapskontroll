e2d33adab7b971eb64dd1cf2c0f941a676c6012a
Anastasia Korzh <anakor192@ihmeducation.se>
2024-09-23 11:40:19 +0200

    update exam

--- js/app.js
+++ js/app.js
@@ -5,25 +5,63 @@ let button0 = document.getElementById("button0");
 // Text
 let playerRollText = document.getElementById("playerRollText");
 let aiRollText = document.getElementById("aiRollText");
-let
+let result = document.getElementById("result");
+let playerScoreText = document.getElementById("playerScoreText");
+let aiScoreText = document.getElementById("aiScoreText");
 
-// Data
+
+ // Data
 let playerRoll = 0;
 let aiRoll = 0;
+let playerScore = 0;
+let aiScore = 0;
+
 
 //PROCESS
 button0.addEventListener("click", function() {
   getRandomNumberOneToSixForPlayer ();
-  showPlayerRollResult ();
-  showAiRollResult
-})
+  showPlayerRollResult();
+  getRandomNumberOneToSixForAi();
+  showAiRollResult();
+  evaluateResult();
+  showPlayerScore();
+  showAiScore();
+});
 
 // CONTROLLERS
-function getRandomNumberOneToSixForPlayer () {
-  playerRoll = Math.floor(Math.random() * 6) +1;
+function getRandomNumberOneToSixForPlayer() {
+  playerRoll = Math.floor(Math.random() * 6) + 1;
 }
+function getRandomNumberOneToSixForAi() {
+  aiRoll = Math.floor(Math.random() * 6) + 1;
+}
+function evaluateResult() {
+  if (playerRoll > aiRoll) {
+    result.innerHTML = "Player wins!"
+    playerScore++;
+  }
+  else if (aiRoll > playerRoll) {
+    result.innerHTML = "Ai Wins!"
+    aiScore++;
+}
+  else {
+
+  }
 
 // VIEWS
-function showPlayerRollResult () {
+function showPlayerRollResult() {
   playerRollText.innerHTML = playerRoll;
 }
+
+function showAiRollResult() {
+  aiRollText.innerHTML = aiRoll;
+}
+function showPlayerScore() {
+    playerScoreText.innerHTML = "Player Score: " + playerScore;
+}
+function showAiScore() {
+  aiScoreText.innerHTML = "Ai Score: " + aiScore;
+}
+
+
+
